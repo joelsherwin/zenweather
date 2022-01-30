5dac3228b20eda61966fb8776b905bbf38a977e5
Joel Sherwin <sherwinjoel@gmail.com>
2022-01-31 01:09:13 +0530

    fix background color for better neumorphics

--- client/src/Stylesheets/WeatherCard.js
+++ client/src/Stylesheets/WeatherCard.js
@@ -4,7 +4,7 @@ export default makeStyles(()=>({
     root: {
         display: "flex",
         flexDirection: "column",
-        backgroundColor: 'white',
+        backgroundColor: '#EBF5FC',
         borderRadius:'30px',
         boxShadow : '-6px -6px 20px rgba(255,255,255,1), 6px 6px 20px rgba(0,0,0,0.1)',
         width:'250px',
